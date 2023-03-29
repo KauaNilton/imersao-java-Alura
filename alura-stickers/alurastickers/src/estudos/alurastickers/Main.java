431b1d36758c8e9454bbac11e1436747fb612201
perkles <kaua4nilton@gmail.com>
2023-03-29 17:35:36 -0300

    removendo codigos não utilizados

--- alura-stickers/alurastickers/src/estudos/alurastickers/Main.java
+++ alura-stickers/alurastickers/src/estudos/alurastickers/Main.java
@@ -25,22 +25,11 @@ public class Main {
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        // exibir e manipular os dados
-       /* for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println("\u001b[1mTítulo: \u001b[93m" + filme.get("title") + "\u001b[0m");
-            System.out.println("\u001b[1mPoster: " + filme.get("image") + "\u001b[0m");
-            System.out.println("\u001b[38;2;255;255;255m\u001b[48;2;9;46;132mClassificação: " + filme.get("imDbRating") + "\u001b[0m");
-            double classificacao = Double.parseDouble(filme.get("imDbRating"));
-            int numeroEstrelas = (int) classificacao;
-            for(int i = 0; i < numeroEstrelas; i++) {
-                System.out.print("\u2B50");
-            }
-
-            System.out.println("\n");
-        }*/
-
+        //criação do repositorio para as figurinhas
         File diretorio = new File("figurinhas/");
         diretorio.mkdir();
+
+        // gerando figurinhas
         var geradora = new GeradorDeFigurinhas();
         for (Map<String, String> filme : listaDeFilmes) {
 
